bc9bef03b46ab46b119e61ba8c701fbb7669abe5
jonasstuds-gaard <jonasstuds@gmail.com>
2025-12-11 15:20:34 +0000

    change test

--- test_push.R
+++ test_push.R
@@ -4,3 +4,4 @@ install.packages("git")
 gdfd # this will trigger a conflict for sure it should at least 
 
 This is additional changes i made 
+there cannot be a conflict with a push as the local branch is "ahead" of the gitbranch 
